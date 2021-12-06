99c36a40cb8c0e5dacd765ddf751bd4f8952fb0e
miguelogren <miguel.h.ogren@gmail.com>
2021-12-06 14:24:14 +0100

    Development started

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,13 @@
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String address;
+    private String nationality;
+    private String idNumber;
+
+    public Person(String idNumber){
+        this.idNumber = idNumber;
+    }
 }
