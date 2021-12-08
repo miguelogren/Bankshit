546e6790b58ef492caef00c557250c374168ec2c
Rothoff <rothoff.fredrik@outlook.com>
2021-12-08 14:41:28 +0100

    Added a GUI acc overview and login

--- src/GUI/AccountOverview.java
+++ src/GUI/AccountOverview.java
@@ -1,4 +1,71 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class AccountOverview {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        JPanel jPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JLabel DailyAcc = new JLabel("Daily acc", SwingConstants.CENTER);
+        JLabel SavingAcc = new JLabel("Saving acc", SwingConstants.CENTER);
+
+        JButton pay = new JButton("Betala");
+        JButton transfer = new JButton("Överför");
+        JButton gamble = new JButton("Spela");
+
+        jFrame.setVisible(true);
+        jFrame.pack();
+        jFrame.add(jPanel);
+        jFrame.setTitle("Bank");
+        jFrame.setSize(500,600);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Color panelColor = new Color(30,120,200);
+        Color labelColor = new Color(30, 140, 250);
+
+        jPanel.setVisible(true);
+        jPanel.setBackground(panelColor);
+
+        DailyAcc.setOpaque(true);
+        DailyAcc.setPreferredSize(new Dimension(475, 100));
+        DailyAcc.setFont(new Font("Arial", Font.BOLD, 32));
+
+        SavingAcc.setOpaque(true);
+        SavingAcc.setPreferredSize(new Dimension(475,100));
+        SavingAcc.setFont(new Font("Arial", Font.BOLD, 32));
+
+        pay.setPreferredSize(new Dimension(475, 100));
+        transfer.setPreferredSize(new Dimension(475, 100));
+        gamble.setPreferredSize(new Dimension(475, 100));
+
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        jPanel.add(DailyAcc, gbc);
+
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        jPanel.add(SavingAcc, gbc);
+
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        jPanel.add(pay, gbc);
+
+        gbc.gridy = 3;
+        gbc.fill = GridBagConstraints.BOTH;
+        jPanel.add(transfer, gbc);
+
+        gbc.gridy = 4;
+        gbc.fill = GridBagConstraints.BOTH;
+        jPanel.add(gamble, gbc);
+
+
+
+    }
 }
+
--- src/GUI/LogIn.java
+++ src/GUI/LogIn.java
@@ -1,4 +1,82 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class LogIn {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        JPanel jPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 20, 30);
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        jFrame.setVisible(true);
+        jFrame.pack();
+        jFrame.add(jPanel);
+        jFrame.setTitle("Login");
+        jFrame.setSize(400,250);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(jPanel);
+
+        jFrame.setVisible(true);
+
+        JLabel username = new JLabel("Username:");
+        JLabel password = new JLabel("Password:");
+
+        JTextArea user = new JTextArea();
+        JTextArea pass = new JTextArea();
+
+        JCheckBox showPassword = new JCheckBox("Show password");
+
+        JButton login = new JButton("Login");
+
+        username.setFont(new Font("Arial", Font.BOLD, 18));
+        username.setOpaque(false);
+
+        user.setFont(new Font("Arial", Font.ITALIC, 16));
+        user.setPreferredSize(new Dimension(150,20));
+        user.setLineWrap(true);
+        //user.append("Username");
+
+        pass.setFont(new Font("Arial", Font.ITALIC, 16));
+        pass.setPreferredSize(new Dimension(150,20));
+        pass.setLineWrap(true);
+        //pass.append("Password");
+
+        password.setFont(new Font("Arial", Font.BOLD, 18));
+        password.setOpaque(false);
+
+        login.setFont(new Font("Arial", Font.ITALIC, 16));
+        login.setPreferredSize(new Dimension(100,30));
+
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        jPanel.add(username, gbc);
+
+        gbc.gridy = 0;
+        gbc.gridx = 1;
+        jPanel.add(user, gbc);
+
+        gbc.gridy = 1;
+        gbc.gridx = 0;
+        jPanel.add(password, gbc);
+
+        gbc.gridy = 1;
+        gbc.gridx = 1;
+        jPanel.add(pass, gbc);
+
+        gbc.gridy = 2;
+        gbc.gridx = 1;
+        jPanel.add(showPassword, gbc);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        jPanel.add(login, gbc);
+
+
+
+    }
 }
