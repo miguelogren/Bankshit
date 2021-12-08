96dcac0e93c7c3894a23688a351847f08e10fc0c
JohanssonJacob <jacobjohanssn2001@gmail.com>
2021-12-08 15:07:30 +0100

    biggest commit ever

--- src/GUI/Window.java
+++ src/GUI/Window.java
@@ -1,4 +1,16 @@
 package GUI;
 
-public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        setTitle("Bank");
+        setSize(400,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
 }
